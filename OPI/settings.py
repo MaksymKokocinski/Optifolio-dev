ca9514338a6b4313fbf206164c88ba00b06c67c3
Maksym Kokocinski <m.kokocinski5050@gmail.com>
2021-10-10 11:10:59 +0200

    Update settings whitenoise

--- OPI/settings.py
+++ OPI/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -143,3 +143,8 @@ EMAIL_HOST_PASSWORD = config('email_host_pass',default='')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
