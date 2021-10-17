07ab2525e327a4f66c591b5265198d2a59fc3ea0
Anita Kosobucka <anita@kosobucka.pl>
2021-10-17 22:29:44 +0200

    connect to heroku database

--- OPI/settings.py
+++ OPI/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import django_heroku
 from pathlib import Path
 from decouple import config
 
@@ -121,8 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
+STATIC_ROOT - os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = "/static/"
+django_heroku.settings(locals())
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
