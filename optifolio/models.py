95184984fe8eb0016bf53b65f7df81411b2015e4
magdabiadala <magdalena.biadala@gmail.com>
2021-11-21 22:33:55 +0100

    Form for adding new portfolio

--- optifolio/models.py
+++ optifolio/models.py
@@ -14,17 +14,22 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 		
+class Portfolio(models.Model):
+    portfolio_id = models.AutoField(primary_key=True,blank=True)
+    portfolio_title = models.CharField(max_length=200, null=True,blank=True)
+    user_name = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL,blank=True)
+
+    def __str__(self):
+        return self.portfolio_title if self.portfolio_title else ''
 
 class VisData(models.Model):
     BUY_SELL_CHOICES = [('+', 'Kupno'), ('-','Sprzedaz')]
     visdata_id = models.AutoField(primary_key=True,blank=True)
-    user_name = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL,blank=True)
+    port_id = models.ForeignKey(Portfolio, null=True, on_delete=models.SET_NULL,blank=True)
     title = models.CharField(max_length=200, null=True,blank=True)
     buy_sell = models.CharField(max_length=8, choices=BUY_SELL_CHOICES, null=True, blank=True)
     date = models.DateField(auto_now_add=False,null=True,editable=True,blank=True)
     hour = models.TimeField(auto_now=False, auto_now_add=False,null=True,editable=True,blank=True)
-    #nie wiem co tu jest nie tak, chcialam dodac ograniczenie wymagajace liczb dodatnich
-    #shares_number = models.DecimalField(decimal_places=8,default=0,max_digits=999, null=True,blank=True, validators=[MinValueValidator(Decimal('0.01'))])
     shares_number = models.DecimalField(decimal_places=8,default=0,max_digits=999, null=True,blank=True)
     course = models.DecimalField(decimal_places=2,default=0,max_digits=999,null=True,blank=True)
     fare = models.DecimalField(decimal_places=2,default=0,max_digits=999,null=True,blank=True)
--- optifolio/views.py
+++ optifolio/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
 from .models import *
-from .forms import CreateUserForm, CustomerForm, AddSharesForm
+from .forms import CreateUserForm, CustomerForm, AddSharesForm, AddPortfolioForm
 from .decorators import unauthenticated_user,allowed_users,admin_only
 
 from django.db.models import Count, Sum, F
@@ -110,7 +110,8 @@ def visualisationPage(request):
             form = AddSharesForm()
 
     #for user restriction 
-    visdata = request.user.customer.visdata_set.all()
+    #visdata = request.user.customer.visdata_set.all()
+    visdata = request.user.customer.portfolio.visdata_set.all()
     #shows all data    
     #visdata = VisData.objects.all()
     context = {'form': form, 'visdata':visdata,'current_user_name':current_user_name}
@@ -120,58 +121,73 @@ def visualisationPage(request):
 @allowed_users(allowed_roles=['customer'])
 def summaryPage(request):
     current_user_name = request.user.customer
-    #print(current_user_name)
+
+    form = AddPortfolioForm()
+    if request.method == 'POST':
+        print("Printing POST: ", request.POST)
+        form = AddPortfolioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('summary')
+    else:
+            form = AddPortfolioForm()
+
+
+
 
     #for user restriction 
-    visdata = request.user.customer.visdata_set.all()
+    #visdata = request.user.customer.visdata_set.all()
+    #visdata = request.user.customer.portfolio.visdata_set.all()
     #shows all data
-    #visdata = VisData.objects.all()
-    comp_number = visdata.count()
-    if comp_number > 0:
+    #comp_number = visdata.count()
+    #if comp_number > 0:
 
-        shares_num = visdata.aggregate(Sum(('shares_number')))
-        shares_num_sum = (shares_num['shares_number__sum'])
-        shares_num_sum = format(shares_num_sum, ".0f")
+     #   shares_num = visdata.aggregate(Sum(('shares_number')))
+      #  shares_num_sum = (shares_num['shares_number__sum'])
+       # shares_num_sum = format(shares_num_sum, ".0f")
 
         #profit_earned = visdata.aggregate(Sum(('course')))
         #profit_sum = (profit_earned['course__sum'])
 
-        fare_paid = visdata.aggregate(Sum(('fare')))
-        fare_sum = (fare_paid['fare__sum'])
+    #    fare_paid = visdata.aggregate(Sum(('fare')))
+     #   fare_sum = (fare_paid['fare__sum'])
         
-        mod_date = visdata.order_by('-date').first().date
+      #  mod_date = visdata.order_by('-date').first().date
         
-        to_buy = visdata.filter(buy_sell='+').count()
-        to_sell = visdata.filter(buy_sell='-').count()
+       # to_buy = visdata.filter(buy_sell='+').count()
+        #to_sell = visdata.filter(buy_sell='-').count()
 
-        to_buy_percentage = 0
-        to_buy_percentage = to_buy / comp_number
-        to_buy_percentage = (to_buy_percentage) * 100
-        to_buy_percentage = format(to_buy_percentage, ".0f")
-        to_buy_percentage = str(to_buy_percentage) + '%'
+    #    to_buy_percentage = 0
+     #   to_buy_percentage = to_buy / comp_number
+      #  to_buy_percentage = (to_buy_percentage) * 100
+       # to_buy_percentage = format(to_buy_percentage, ".0f")
+        #to_buy_percentage = str(to_buy_percentage) + '%'
         
         #for customer restriction delete object and change VisData to visdata
-        aggregated_data = visdata.annotate(
-        intermid_result=F('course') - F('fare')
-        ).annotate(
-        record_total=F('shares_number') * F('intermid_result')
-        ).aggregate(
-        total=Sum('record_total')
-        )
-        profit_earned = aggregated_data['total']
-        profit_earned = format(profit_earned, ".2f")
+    #    aggregated_data = visdata.annotate(
+     #   intermid_result=F('course') - F('fare')
+      #  ).annotate(
+       # record_total=F('shares_number') * F('intermid_result')
+        #).aggregate(
+     #   total=Sum('record_total')
+      #  )
+       # profit_earned = aggregated_data['total']
+        #profit_earned = format(profit_earned, ".2f")
 
-    else:
-        shares_num_sum = 0
-        fare_sum = 0
-        mod_date = 'brak'
-        to_buy = 0
-        to_sell = 0
-        to_buy_percentage = 0
-        profit_earned = 0
+    #else:
+     #   shares_num_sum = 0
+      #  fare_sum = 0
+       # mod_date = 'brak'
+        #to_buy = 0
+      #  to_sell = 0
+       # to_buy_percentage = 0
+        #profit_earned = 0
     
-    context = {'comp_number': comp_number, 'shares_num':shares_num_sum,'to_buy_percentage':to_buy_percentage,
-     'profit_earned': profit_earned, 'fare_sum':fare_sum,'mod_date':mod_date,'current_user_name':current_user_name}
+    #context = {'comp_number': comp_number, 'shares_num':shares_num_sum,'to_buy_percentage':to_buy_percentage,
+     #'profit_earned': profit_earned, 'fare_sum':fare_sum,'mod_date':mod_date,'current_user_name':current_user_name}
+    
+    context = {'form': form}
+     
     return render(request, 'optifolio/summary.html',context)
 
 @unauthenticated_user
