8ef8c81094f6ae2fec4c62b205a8cbb920b42b8e
Anita Kosobucka <anita@kosobucka.pl>
2021-12-02 09:56:10 +0100

    add ticker_name model

--- optifolio/models.py
+++ optifolio/models.py
@@ -32,3 +32,11 @@ class VisData(models.Model):
 
     def __str__(self):
         return self.title if self.title else ''
+
+class TicName(models.Model):
+    tic_id = models.AutoField(primary_key=True,blank=True)
+    tic_sym = models.CharField(max_length=10,null=False,blank=False)
+    tic_name = models.CharField(max_length=100,null=False,blank=True)
+
+    def __str__(self) :
+        return self.tic_name 
