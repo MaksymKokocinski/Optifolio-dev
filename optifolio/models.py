6eb4a4278cede938caa88360c4f0086d88360d6c
Anita Kosobucka <anita@kosobucka.pl>
2021-12-04 13:54:36 +0100

    add company choices

--- optifolio/models.py
+++ optifolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 #from djmoney.models.fields import MoneyField
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from yahoo_fin.stock_info import tickers_sp500
 
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True,blank=True, on_delete=models.CASCADE)
@@ -14,12 +15,23 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 		
+class TicName(models.Model):
+    tic_sym = models.CharField(max_length=10,null=False,blank=False)
+    tic_name = models.CharField(max_length=100,null=False,blank=True)
+
+    def __str__(self) :
+        return self.tic_name 
 
 class VisData(models.Model):
+    list_names = []
+    for i in tickers_sp500():
+        list_names.append((i,i))
     BUY_SELL_CHOICES = [('+', 'Kupno'), ('-','Sprzedaz')]
     visdata_id = models.AutoField(primary_key=True,blank=True)
     user_name = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL,blank=True)
-    title = models.CharField(max_length=200, null=True,blank=True)
+    TITLE_CHOICES = list_names
+    title = title = models.CharField(max_length=200, null=True,blank=True)
+    title2 = models.ForeignKey(TicName,max_length=200, choices = TITLE_CHOICES,on_delete=models.SET_NULL, null=True,blank=True)
     buy_sell = models.CharField(max_length=8, choices=BUY_SELL_CHOICES, null=True, blank=True)
     date = models.DateField(auto_now_add=False,null=True,editable=True,blank=True)
     hour = models.TimeField(auto_now=False, auto_now_add=False,null=True,editable=True,blank=True)
@@ -28,15 +40,7 @@ class VisData(models.Model):
     shares_number = models.DecimalField(decimal_places=8,default=0,max_digits=999, null=True,blank=True)
     course = models.DecimalField(decimal_places=2,default=0,max_digits=999,null=True,blank=True)
     fare = models.DecimalField(decimal_places=2,default=0,max_digits=999,null=True,blank=True)
-
+    
 
     def __str__(self):
         return self.title if self.title else ''
-
-class TicName(models.Model):
-    tic_id = models.AutoField(primary_key=True,blank=True)
-    tic_sym = models.CharField(max_length=10,null=False,blank=False)
-    tic_name = models.CharField(max_length=100,null=False,blank=True)
-
-    def __str__(self) :
-        return self.tic_name 
