908a0654ba083be9a47d9a2747507c2bb022af5c
Anita Kosobucka <anita@kosobucka.pl>
2022-01-12 20:40:10 +0100

    updated optimalization.py file

--- optimalization_jupyter/optimalization.py
+++ optimalization_jupyter/optimalization.py
@@ -15,89 +15,73 @@ portfolio = pd.read_csv('test.txt', sep=';')
 print(portfolio)
 three_yrs_ago = date.today() - relativedelta(years=3)
 returns = pd.DataFrame()
-mean_return = pd.DataFrame()
-mean_std = pd.DataFrame()
 exc_returns = pd.DataFrame()
 sharpe_ratio = pd.DataFrame()
-for i in spolki:
-    price = get_data(ticker=i,start_date=three_yrs_ago,interval="1mo",index_as_date=False)
-    price = price[['close','date']]
-    new_df = price.diff(axis=0)
-    price['diff'] = new_df['close']
-    price['shifted'] = price['close'].shift(periods=1)
-    price['return'] = price['diff']/price['shifted']
-    price.iloc[[-1],[4]] = np.nan
-    price.drop([0],inplace=True)
-    price.drop(price.tail(1).index,inplace=True)
 
-    returns[i] = price['return']
-    
-    #tabela średniego zwrotu i średniego rocznego zwrotu
-    
-#print(returns)
-
-
-mean_return['średni zwrot'] = returns.mean()
-
-#mean_return['roczny średni zwrot'] = returns.mean()*12 
-#print(mean_return)
-#mean_return = mean_return.transpose()
-mean_return_series = mean_return.squeeze()
-
-#tabela excess zwrotów
-exc_returns = returns.sub(mean_return_series, axis=1)
-exc_returns = exc_returns.to_numpy()
-#print(exc_returns)
-exc_returns_trans = exc_returns.transpose()
-#print(exc_returns_trans)
-#macierz kowariancji pomnożeznie macierzy ex zwrotów z transponowaną nią samą a potem podzielenie przez ilość dat(liczba rzędów tabeli exc return)
-#var_cov = exc_returns.mul(exc_returns.transpose())
-#print(var_cov)
-x = exc_returns.shape[0]
-var_cov = np.matmul(exc_returns_trans,exc_returns)
-#print(var_cov)
-var_cov = np.divide(var_cov,x)
-print(var_cov)
-
-#średnie odchylenie
-std_dev = pd.DataFrame()
-std_dev['std_dev'] = returns.std()
-print(std_dev)
-sharpe_ratio["sharpe_ratio"] = returns.mean()/returns.std()
-
-print(sharpe_ratio)
 
 portfolio['wartość'] = portfolio['liczba_akcji'] * portfolio['obecny kurs']
-
-#aktualna wartość portfolio
-portfolio_value=portfolio['wartość'].sum()
-
-portfolio['procent wartosci'] = portfolio['wartość']/portfolio_value
+portfolio_value=portfolio['wartość'].sum() #aktualna wartość portfolio
+portfolio['procent wartosci'] = portfolio['wartość']/portfolio_value #procent wartości danej akcji w całym portfleu
 wagi = portfolio['procent wartosci'].to_numpy()
 
-#portfolio standard deviation
-portfolio_std_dev=np.sqrt(np.matmul(np.matmul(wagi,var_cov),wagi))
-print('portfolio standard deviation',portfolio_std_dev)
+def monthly_returns(companies_list):
+    for i in companies_list:
+        price = get_data(ticker=i,start_date=three_yrs_ago,interval="1mo",index_as_date=False)
+        price = price[['close','date']]
+        new_df = price.diff(axis=0)
+        price['diff'] = new_df['close']
+        price['shifted'] = price['close'].shift(periods=1)
+        price['return'] = price['diff']/price['shifted']
+        price.iloc[[-1],[4]] = np.nan
+        price.drop([0],inplace=True)
+        price.drop(price.tail(1).index,inplace=True)
 
-#portfolio expected return
-portfolio_exp_return=(returns.mean()*wagi).sum()
-prt_2 = np.matmul(returns.mean(),wagi)
-print('portfolio eexpected return',portfolio_exp_return, prt_2)
+        returns[i] = price['return']
+    return returns 
 
-#w tabeli potrzebuje: nazwę firmy, oczekiwane zwroty(na podstawie historii, odchylenie standardowe, wagi) 
-#na podstawie oczekiwanych wzrotów /odchylenie standardowe  licze sharpe_ratio dla każdej akcji
-new =  pd.DataFrame()
-new['name'] = spolki
-new['exp_ret'] = mean_return['średni zwrot']
-max_expected_return = max(mean_return['średni zwrot'])
+returns = monthly_returns(spolki)
+sharpe_ratio["sharpe_ratio"] = returns.mean()/returns.std()
+#funkcja licząca macierz kowariancji
+def variance_covariance_matrix(mean_returns_dataframe):
+    mean_return_series = mean_returns_dataframe.squeeze()
+    exc_returns = returns.sub(mean_return_series, axis=1)
+    exc_returns = exc_returns.to_numpy()
+    exc_returns_trans = exc_returns.transpose()
+    var_cov = np.matmul(exc_returns_trans,exc_returns)
+    var_cov = np.divide(var_cov,exc_returns.shape[0])
+    return var_cov
+
+
+#funkcja która liczy odchylenie standardowe w zależności od wag
+def get_portfolio_std_deviation(weights):
+    portfolio_standard_deviation = np.sqrt(np.matmul(np.matmul(weights,variance_covariance_matrix(returns.mean())), weights))
+    return portfolio_standard_deviation     
+
+#funkcja która liczy oczekiwany zwrot portfolio w zależności od wag
+def get_portfolio_exp_return(weights):
+    portfolio_exp_return = np.matmul(returns.mean(), weights)
+    return portfolio_exp_return
+
+def get_portfolio_sharpe_ratio(weights):
+    portfolio_sharpe_ratio = get_portfolio_exp_return(weights)/get_portfolio_std_deviation(weights)
+    return portfolio_sharpe_ratio
+
+
+max_expected_return = max(returns.mean())
 min_std_dev = min(returns.std())
-#print(returns.std())
-#print(min_std_dev)
 max_sharpe_ratio = max(sharpe_ratio['sharpe_ratio'])
 
-print(max_expected_return,min_std_dev,max_sharpe_ratio)
-print('portfolio sharpe ratio', portfolio_exp_return/portfolio_std_dev)
+# bnds = ((0, None), (0, None), (0, None), (0, None), (0, None), (0, None), (0, None), (0, None))
+# cons = ({'type': 'eq', 'fun': lambda x:  sum(x) - 1},
+#         {'type': 'ineq', 'fun': lambda x: min_std_dev - get_portfolio_std_deviation(x)})
 
-print('covariance matrix', var_cov)
-print('wagi', wagi)
+# result = opt.minimize(get_portfolio_exp_return, wagi, bounds=bnds, constraints=cons)
+# print(result)
+cons = ({'type': 'eq', 'fun': lambda x:  sum(x) - 1},
+        {'type': 'ineq', 'fun': lambda x: get_portfolio_exp_return(x) - max_expected_return})
+
+result = opt.minimize(get_portfolio_std_deviation, wagi, constraints=cons)
+print(result.x)
+print(result.fun)
+print(sum(result.x))
 
